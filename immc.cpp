18b433efc9d84f2475e4f3b6f12617da33599d3e
Martin Munilla <martinmunillas@gmail.com>
2020-10-16 23:27:06 -0300

    ⛴ First draft of the first problem code

---
+++ immc.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct vehicle {
+    string name;
+    float length;
+};
+
+int main() {
+    int G, H, L;
+    vector<vehicle> vehicles;
+    cout << "Ingrese la ganancia minima necesaria: ";
+    cin >> G;
+    cout << endl << "Ingrese el largo del Ferry: ";
+    cin >> L;
+    cout << endl << "Ingrese la cantidad de hileras del Ferry: ";
+    cin >> H;
+    while(true) {
+        cout << endl << "Ingrese 'a' para agregar vehiculos o 'e' para dejar de agregar vehiculos: ";
+        char k;
+        cin >> k;
+        if (k == 'a') {
+            cout << endl << "Ingrese un tipo de vehiculo del cual quiere saber el precio: ";
+            vehicle x;
+            cin >> x.name;
+            cout << endl << "Ingrese el largo tipo de vehiculo: ";
+            cin >> x.length;
+            vehicles.push_back(x);
+        } else if (k == 'e') {
+            break;
+        } else {
+            cout << endl << "Comando invalido";
+            continue;
+        }
+    }
+
+    for (int i = 0; i < vehicles.size(); i++) {
+        int P;
+        P = G / ((L / vehicles[i].length) * H);
+        cout << "El precio de " << vehicles[i].name << " debiera ser: $" << P << endl;
+
+    }
+       
+}
